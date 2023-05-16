0d578b3992622b4908fceff0b3c4d03a72da0d94
ahmedkhaleel <duZhuqkyptu8barnuz>
2023-05-16 22:06:06 +0300

    Laravel Breeze with User Role Areas

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,6 +39,20 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <!-- Role -->
+        <div class="mt-4">
+            <x-input-label for="role_id" :value="__('Regestration as :')" />
+
+            <label class="dark:text-white">
+                Student
+                <input name="role_id" value="1" type="radio">
+            </label>
+            <label class="dark:text-white px-4">
+                teacher
+                <input name="role_id" value="2" type="radio">
+            </label>
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
